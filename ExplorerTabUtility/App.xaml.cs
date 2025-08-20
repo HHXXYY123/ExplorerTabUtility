312d2637160e9eb975fb1dde8e23220eec9995ad
HHXXYY123 <1340865399@qq.com>
2025-08-20 11:51:01 +0800

    修复：恢复正确的应用程序初始化逻辑

--- ExplorerTabUtility/App.xaml.cs
+++ ExplorerTabUtility/App.xaml.cs
@@ -1,49 +1,29 @@
-using System.Windows;
 using System.Threading;
-using System.Windows.Controls;
-using ExplorerTabUtility.UI.Views;
-using ExplorerTabUtility.Helpers;
-using ExplorerTabUtility.Managers;
+using System.Windows;
 
-namespace ExplorerTabUtility;
-
-// ReSharper disable once RedundantExtendsListEntry
-public partial class App : Application
+namespace ExplorerTabUtility
 {
-    private Mutex? _mutex;
-
-    protected override void OnStartup(StartupEventArgs e)
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
     {
-        Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
-        _mutex = new Mutex(true, Constants.MutexId, out var createdNew);
+        private Mutex _mutex;
 
-        if (createdNew)
+        protected override void OnStartup(StartupEventArgs e)
         {
+            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+            
+            _mutex = new Mutex(true, "ExplorerTabUtility", out var createdNew);
+
+            if (!createdNew)
+            {
+                Current.Shutdown();
+                return;
+            }
+
             base.OnStartup(e);
-            SetupTooltipBehavior();
-            _ = new HookManager();
-            _ = new MainWindow();
-            return;
+            _ = new UI.Views.MainWindow();
         }
-
-        CustomMessageBox.Show("""
-                              Another instance is already running.
-                              Check in System Tray Icons.
-                              """, Constants.AppName, icon: MessageBoxImage.Information);
-        Shutdown();
-    }
-
-    protected override void OnExit(ExitEventArgs e)
-    {
-        _mutex?.Dispose();
-        base.OnExit(e);
-    }
-
-    private static void SetupTooltipBehavior()
-    {
-        ToolTipService.ShowDurationProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(3500));
-        ToolTipService.InitialShowDelayProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(1700));
-        ToolTipService.BetweenShowDelayProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(150));
-        ToolTipService.ShowsToolTipOnKeyboardFocusProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(false));
     }
 }
